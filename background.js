64552c71bcea7cbb5ef7d947d45dce814a814b55
sepmaes <seppe.maes@ugent.be>
2021-01-03 22:53:14 +0100

    initial commit

---
+++ background.js
@@ -0,0 +1,71 @@
+var loading = false;
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.browserAction.setBadgeText({text: "On"});
+    chrome.storage.sync.set({"state": true});
+    var rule = {
+        conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { hostSuffix: 'ufora.ugent.be' },
+            })
+        ],
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      };
+    
+      chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([rule]);
+    });
+});
+
+
+
+// Knop listener
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.storage.sync.get("state", function(result){
+        if (result.state) {
+            chrome.browserAction.setBadgeText({text: ""});
+        } else {
+            chrome.browserAction.setBadgeText({text: "On"});
+        }
+        chrome.storage.sync.set({"state": !result.state});
+        changecss(tab);
+    });  
+});
+
+// Page refresh listener
+chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
+    if (info.status == "loading") {
+        if (! loading) {
+            changecss(tab);
+            loading = true;
+        } else {
+
+        }
+        loading = true;
+    } else {
+        loading = false;
+    }
+    
+});
+
+// Apply/remove dark.css
+function changecss(tab) {
+    if (tab.url.includes("://ufora.ugent.be/")) {
+        chrome.tabs.removeCSS({ file: "dark.css" });
+        chrome.storage.sync.get("state", function(result){
+            if (result.state) {
+                chrome.tabs.insertCSS({ file: "dark.css" });
+                cssScript(tab);
+            }
+        });  
+    }
+}
+
+function cssScript(tab) {
+    // Accoutninstellingen - background
+    if (tab.url.includes("://ufora.ugent.be/d2l/lp/preferences")){
+        chrome.tabs.executeScript({
+            code: 'var all = document.getElementsByClassName("dco_c"); for (var i = 0; i < all.length; i++) { all[i].style.backgroundColor = "#202020"; }'
+        });
+    } 
+}
