8cbb643da53d8710e4f330caed6069d789201aa5
sepmaes <seppe.maes@ugent.be>
2021-01-04 23:51:11 +0100

    Loginpage darkmode, imporved darkmode

--- background.js
+++ background.js
@@ -2,16 +2,18 @@ var loading = false;
 
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({"state": true});
-    chrome.storage.sync.set({"colors": {"background": "#ff0000",
+    chrome.storage.sync.set({"sites": {"login": true}});
+    chrome.storage.sync.set({"colors": {"background": "#000000",
                                         "widgetbackground": "#202020",
                                         "selected": "#353535",
                                         "lines": "#999999",
                                         "accent": "#0077ff",
                                         "accent2": "#00ccff",
+                                        "textaccent": "#ffffff",
                                         "text": "#eeeeee",
                                         "title": "#0084ff",
-                                        "link": "#ffef0a",
-                                        "linkvisited": "#77700f"}});
+                                        "link": "#00bbff",
+                                        "linkvisited": "#0095ff"}});
     var rule = {
         conditions: [
             new chrome.declarativeContent.PageStateMatcher({
@@ -26,22 +28,13 @@ chrome.runtime.onInstalled.addListener(function() {
     });
 });
 
-
-
-// Knop listener
-// chrome.browserAction.onClicked.addListener(function (tab) {
-//     chrome.storage.sync.get("state", function(result){
-//         if (result.state) {
-//             chrome.browserAction.setBadgeText({text: ""});
-//         } else {
-//             chrome.browserAction.setBadgeText({text: "On"});
-//         }
-//         chrome.storage.sync.set({"state": !result.state});
-//         changecss(tab);
-//     });  
-// });
-
 // Page refresh listener
+chrome.tabs.onActiveChanged.addListener(function (tabId, slectInfo) {
+    chrome.tabs.getSelected(null, function(tab) {
+        changecss(tab);
+    });
+});
+
 chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
     if (info.status == "loading") {
         if (! loading) {
@@ -58,28 +51,70 @@ chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
     
 });
 
-// Apply/remove dark.css
+// Storage listener
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    chrome.tabs.getSelected(null, function(tab) {        
+        if (changes.login) {
+            if (tab.url.includes("://elosp.ugent.be/") || tab.url.includes("://login.ugent.be/")) {
+                changecss(tab);
+            }
+        } else {
+            changecss(tab);
+        }
+    });
+});
+
+// Apply/remove css
 function changecss(tab) {
-    if (tab.url.includes("://ufora.ugent.be/")) {
-        chrome.tabs.removeCSS({ file: "dark.css" });
+    if (tab.url && tab.url.includes("://ufora.ugent.be/")) {
+        chrome.tabs.removeCSS({ file: "ufora.css" });
         chrome.storage.sync.get("state", function(result){
             if (result.state) {
-                chrome.tabs.insertCSS({ file: "dark.css" });
+                chrome.tabs.insertCSS({ file: "ufora.css" });
                 cssScript(tab);
             }
         });  
     }
+    
+    else if (tab.url && tab.url.includes("://elosp.ugent.be/")) {
+        chrome.tabs.removeCSS({ file: "elosp.css" });
+        chrome.storage.sync.get(["state", "sites"], function(result){
+            if (result.state && result.sites.login) {
+                chrome.tabs.insertCSS({ file: "elosp.css" });
+                cssScript(tab);
+            }
+        }); 
+    }
+
+    else if (tab.url && (tab.url.includes("://login.ugent.be/"))) {
+        chrome.tabs.removeCSS({ file: "login.css" });
+        chrome.storage.sync.get(["state", "sites"], function(result){
+            if (result.state && result.sites.login) {
+                chrome.tabs.insertCSS({ file: "login.css" });
+                cssScript(tab);
+            }
+        }); 
+    }
 }
 
 function cssScript(tab) {
-    // Accoutninstellingen - background
-    if (tab.url.includes("://ufora.ugent.be/d2l/lp/preferences")){
-        chrome.tabs.executeScript({
-            code: 'var all = document.getElementsByClassName("dco_c"); for (var i = 0; i < all.length; i++) { all[i].style.backgroundColor = "#202020"; }'
-        });
-    } 
-    
+    // Accoutninstellingen - background    
     chrome.storage.sync.get("colors", function(result) {
+        if (tab.url.includes("://ufora.ugent.be/d2l/lp/preferences")){
+            chrome.tabs.executeScript({
+                code: 'var all = document.getElementsByClassName("dco_c"); for (var i = 0; i < all.length; i++) { all[i].style.backgroundColor = "#202020"; }'
+            });
+        } 
+        // if (tab.url.includes("://ufora.ugent.be/d2l/le/calendar")){
+        //     chrome.tabs.executeScript({
+        //         code: 'var all = document.getElementsByClassName("d2l-le-calendar-today"); for (var i = 0; i < all.length; i++) { all[i] = document.createElement("style"); all[i].type = "text/css"; all[i].id = "styles_js"; document.getElementsByTagName("head")[0].appendChild(all[i]); all[i].appendChild(document.createTextNode(".d2l-le-calendar-today {background-color: red !important;}")); }'
+        //     });
+        // } 
+
+        // witte vakken bij vakken verwijderen
+        chrome.tabs.executeScript({
+            code: "var elements = document.getElementsByClassName('d2l-widget d2l-tile d2l-widget-padding-full'); while(elements.length > 0){ elements[0].parentNode.removeChild(elements[0]);}"
+        });
         var command = "var r = document.querySelector(':root'); "; 
         command += "r.style.setProperty('--background','" + result.colors.background + "');";
         command += "r.style.setProperty('--widget-background','" + result.colors.widgetbackground + "');";
@@ -87,13 +122,14 @@ function cssScript(tab) {
         command += "r.style.setProperty('--lines','" + result.colors.lines + "');";
         command += "r.style.setProperty('--accent','" + result.colors.accent + "');";
         command += "r.style.setProperty('--accent2','" + result.colors.accent2 + "');";
+        command += "r.style.setProperty('--text-accent','" + result.colors.textaccent + "');";
         command += "r.style.setProperty('--text','" + result.colors.text + "');";
         command += "r.style.setProperty('--text-title','" + result.colors.title + "');";
         command += "r.style.setProperty('--text-link','" + result.colors.link + "');";
         command += "r.style.setProperty('--text-link-visited','" + result.colors.linkvisited + "');";
+        command += "r.style.setProperty('--d2l-dropdown-background-color,','" + result.colors.widgetbackground + "');";
         chrome.tabs.executeScript({
             code: command
         });
-    });
-    
+    }); 
 }
--- popup.js
+++ popup.js
@@ -1,34 +1,89 @@
-var swit = document.getElementById("checkbox");
+var enable = document.getElementById("enable");
 
-chrome.storage.sync.get("state", function(result){
-    swit.checked = result.state;
-});
+if (enable) {
+    chrome.storage.sync.get("state", function(result){
+        if (result.state) {
+            enable.textContent = "Disable";
+        } else {
+            enable.textContent = "Enable";
+        }
+    });
 
-if (swit) {
-    swit.addEventListener("change", function() {
-        chrome.storage.sync.set({"state": this.checked});
-        chrome.tabs.getSelected(null, function(tab) {
-            changecss(tab);
-        });
+    enable.addEventListener("click", function() {
+        if (enable.textContent === "Enable") {
+            chrome.storage.sync.set({"state": true});
+            enable.textContent = "Disable";
+        } else {
+            chrome.storage.sync.set({"state": false});
+            enable.textContent = "Enable";
+        }
     });
 }
 
-// Colorselectors
-var cbackground = document.getElementById("cbackground");
+var reset = document.getElementById("reset");
+
+if (reset) {
+    reset.addEventListener("click", function () {
+        chrome.storage.sync.set({"colors": {"background": "#000000",
+                                            "widgetbackground": "#202020",
+                                            "selected": "#353535",
+                                            "lines": "#999999",
+                                            "accent": "#0077ff",
+                                            "accent2": "#00ccff",
+                                            "textaccent": "#ffffff",
+                                            "text": "#eeeeee",
+                                            "title": "#0084ff",
+                                            "link": "#00bbff",
+                                            "linkvisited": "#0095ff"}});
+        initColors();
+    });
+}
+
+var login = document.getElementById("login");
+
+if (login) {
+    chrome.storage.sync.get("sites", function(result){
+        login.checked = result.sites.login;
+    });
+
+    login.addEventListener("change", function(e) {
+        chrome.storage.sync.get("sites", function(result){
+            result.sites.login = login.checked;
+            chrome.storage.sync.set({"sites": result.sites});
+        });
+    })
+}
+
+// Storage listener
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    chrome.storage.sync.get("state", function(result){
+        if (result.state) {
+            document.body.style.backgroundColor = "black";
+            document.body.style.color = "white";
+        } else {
+            document.body.style.backgroundColor = "white";
+            document.body.style.color = "black";
+        }
+    });
+});
 
 // init
-chrome.storage.sync.get("colors", function(result) {
-    cbackground.value = result.colors.background;
-    cwidgetbackground.value = result.colors.widgetbackground;
-    cselected.value = result.colors.selected;
-    clines.value = result.colors.lines;
-    caccent.value = result.colors.accent;
-    caccent2.value = result.colors.accent2;
-    ctext.value = result.colors.text;
-    ctitle.value = result.colors.title;
-    clink.value = result.colors.link;
-    clinkvisited.value = result.colors.linkvisited;
-});
+function initColors() {
+    chrome.storage.sync.get("colors", function(result) {
+        cbackground.value = result.colors.background;
+        cwidgetbackground.value = result.colors.widgetbackground;
+        cselected.value = result.colors.selected;
+        clines.value = result.colors.lines;
+        caccent.value = result.colors.accent;
+        caccent2.value = result.colors.accent2;
+        ctextaccent.value = result.colors.textaccent;
+        ctext.value = result.colors.text;
+        ctitle.value = result.colors.title;
+        clink.value = result.colors.link;
+        clinkvisited.value = result.colors.linkvisited;
+    });
+}
+initColors();
 
 
 // Listeners
@@ -36,139 +91,71 @@ cbackground.addEventListener("change", function() {
     chrome.storage.sync.get("colors", function(result) {
         result.colors.background = cbackground.value;
         chrome.storage.sync.set({"colors": result.colors});
-        chrome.tabs.getSelected(null, function(tab) {
-            changecss(tab);
-        });
     });
 });
 cwidgetbackground.addEventListener("change", function() {
     chrome.storage.sync.get("colors", function(result) {
         result.colors.widgetbackground = cwidgetbackground.value;
         chrome.storage.sync.set({"colors": result.colors});
-        chrome.tabs.getSelected(null, function(tab) {
-            changecss(tab);
-        });
     });
 });
 cselected.addEventListener("change", function() {
     chrome.storage.sync.get("colors", function(result) {
         result.colors.selected = cselected.value;
         chrome.storage.sync.set({"colors": result.colors});
-        chrome.tabs.getSelected(null, function(tab) {
-            changecss(tab);
-        });
     });
 });
 clines.addEventListener("change", function() {
     chrome.storage.sync.get("colors", function(result) {
         result.colors.lines = clines.value;
         chrome.storage.sync.set({"colors": result.colors});
-        chrome.tabs.getSelected(null, function(tab) {
-            changecss(tab);
-        });
     });
 });
 caccent.addEventListener("change", function() {
     chrome.storage.sync.get("colors", function(result) {
         result.colors.accent = caccent.value;
         chrome.storage.sync.set({"colors": result.colors});
-        chrome.tabs.getSelected(null, function(tab) {
-            changecss(tab);
-        });
     });
 });
 caccent2.addEventListener("change", function() {
     chrome.storage.sync.get("colors", function(result) {
         result.colors.accent2 = caccent2.value;
         chrome.storage.sync.set({"colors": result.colors});
-        chrome.tabs.getSelected(null, function(tab) {
-            changecss(tab);
-        });
     });
 });
 cwidgetbackground.addEventListener("change", function() {
     chrome.storage.sync.get("colors", function(result) {
         result.colors.widgetbackground = cwidgetbackground.value;
         chrome.storage.sync.set({"colors": result.colors});
-        chrome.tabs.getSelected(null, function(tab) {
-            changecss(tab);
-        });
+    });
+});
+ctextaccent.addEventListener("change", function() {
+    chrome.storage.sync.get("colors", function(result) {
+        result.colors.textaccent = ctextaccent.value;
+        chrome.storage.sync.set({"colors": result.colors});
     });
 });
 ctext.addEventListener("change", function() {
     chrome.storage.sync.get("colors", function(result) {
         result.colors.text = ctext.value;
         chrome.storage.sync.set({"colors": result.colors});
-        chrome.tabs.getSelected(null, function(tab) {
-            changecss(tab);
-        });
     });
 });
 ctitle.addEventListener("change", function() {
     chrome.storage.sync.get("colors", function(result) {
         result.colors.title = ctitle.value;
         chrome.storage.sync.set({"colors": result.colors});
-        chrome.tabs.getSelected(null, function(tab) {
-            changecss(tab);
-        });
     });
 });
 clink.addEventListener("change", function() {
     chrome.storage.sync.get("colors", function(result) {
         result.colors.link = clink.value;
         chrome.storage.sync.set({"colors": result.colors});
-        chrome.tabs.getSelected(null, function(tab) {
-            changecss(tab);
-        });
     });
 });
 clinkvisited.addEventListener("change", function() {
     chrome.storage.sync.get("colors", function(result) {
         result.colors.linkvisited = clinkvisited.value;
         chrome.storage.sync.set({"colors": result.colors});
-        chrome.tabs.getSelected(null, function(tab) {
-            changecss(tab);
-        });
     });
 });
-
-
-// Apply/remove dark.css
-function changecss(tab) {
-    if (tab.url.includes("://ufora.ugent.be/")) {
-        chrome.tabs.removeCSS({ file: "dark.css" });
-        chrome.storage.sync.get("state", function(result){
-            if (result.state) {
-                chrome.tabs.insertCSS({ file: "dark.css" });
-                cssScript(tab);
-            }
-        });  
-    }
-}
-
-function cssScript(tab) {
-    // Accoutninstellingen - background
-    if (tab.url.includes("://ufora.ugent.be/d2l/lp/preferences")){
-        chrome.tabs.executeScript({
-            code: 'var all = document.getElementsByClassName("dco_c"); for (var i = 0; i < all.length; i++) { all[i].style.backgroundColor = "#202020"; }'
-        });
-    } 
-    
-    chrome.storage.sync.get("colors", function(result) {
-        var command = "var r = document.querySelector(':root'); "; 
-        command += "r.style.setProperty('--background','" + result.colors.background + "');";
-        command += "r.style.setProperty('--widget-background','" + result.colors.widgetbackground + "');";
-        command += "r.style.setProperty('--selected','" + result.colors.selected + "');";
-        command += "r.style.setProperty('--lines','" + result.colors.lines + "');";
-        command += "r.style.setProperty('--accent','" + result.colors.accent + "');";
-        command += "r.style.setProperty('--accent2','" + result.colors.accent2 + "');";
-        command += "r.style.setProperty('--text','" + result.colors.text + "');";
-        command += "r.style.setProperty('--text-title','" + result.colors.title + "');";
-        command += "r.style.setProperty('--text-link','" + result.colors.link + "');";
-        command += "r.style.setProperty('--text-link-visited','" + result.colors.linkvisited + "');";
-        chrome.tabs.executeScript({
-            code: command
-        });
-    });
-    
-}
