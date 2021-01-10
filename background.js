7360ba256a3510d8ffbf5aec7c191e88ae65c42f
Seppe <seppe.maes@ugent.be>
2021-01-10 14:38:58 +0100

    restructured

--- background.js
+++ background.js
@@ -26,13 +26,6 @@ chrome.runtime.onInstalled.addListener(function() {
     
 });
 
-// Page refresh listener
-// chrome.tabs.onActiveChanged.addListener(function (tabId, selectInfo) {
-//     chrome.tabs.getSelected(null, function(tab) {
-//         changecss(tab);
-//     });
-// });
-
 var loadingTabs = {};
 
 chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
@@ -112,25 +105,27 @@ function removeAllCSS() {
 
 // Voeg gegeven css file toe aan de gegeven tab
 function addCSS(tab, cssFile) {
-    chrome.tabs.insertCSS(tab.id, { file: cssFile}, _=>chrome.runtime.lastError);
+    chrome.tabs.insertCSS(tab.id, { file: "css/" + cssFile});
     cssScript(tab);
     dark[tab.id] = cssFile;
 }
 
 // Verwijder gegeven css file van gegeven tab
 function removeCSS(tab, cssFile) {
-    chrome.tabs.removeCSS(tab.id, { file: cssFile}, _=>chrome.runtime.lastError);
+    chrome.tabs.removeCSS(tab.id, { file: "css/" + cssFile});
     delete dark[tab.id];
 }
 
 function cssScript(tab) {
-    // Accoutninstellingen - background    
+    // Accountinstellingen - background    
     chrome.storage.sync.get("colors", function(result) {
         if (tab.url.includes("://ufora.ugent.be/d2l/lp/preferences")){
             chrome.tabs.executeScript(tab.id, {
                 code: 'var all = document.getElementsByClassName("dco_c"); for (var i = 0; i < all.length; i++) { all[i].style.backgroundColor = "#202020"; }'
-            }, _=>chrome.runtime.lastError);
+            });
         } 
+
+        // Werkt niet hoe het moet
         // if (tab.url.includes("://ufora.ugent.be/d2l/le/calendar")){
         //     chrome.tabs.executeScript({
         //         code: 'var all = document.getElementsByClassName("d2l-le-calendar-today"); for (var i = 0; i < all.length; i++) { all[i] = document.createElement("style"); all[i].type = "text/css"; all[i].id = "styles_js"; document.getElementsByTagName("head")[0].appendChild(all[i]); all[i].appendChild(document.createTextNode(".d2l-le-calendar-today {background-color: red !important;}")); }'
@@ -140,7 +135,7 @@ function cssScript(tab) {
         // witte vakken bij vakken verwijderen
         chrome.tabs.executeScript(tab.id, {
             code: "var elements = document.getElementsByClassName('d2l-widget d2l-tile d2l-widget-padding-full'); while(elements.length > 0){ elements[0].parentNode.removeChild(elements[0]);}"
-        }, _=>chrome.runtime.lastError);
+        });
         var command = "var r = document.querySelector(':root'); "; 
         command += "r.style.setProperty('--background','" + result.colors.background + "');";
         command += "r.style.setProperty('--widget-background','" + result.colors.widgetbackground + "');";
@@ -152,9 +147,8 @@ function cssScript(tab) {
         command += "r.style.setProperty('--text-title','" + result.colors.title + "');";
         command += "r.style.setProperty('--text-link','" + result.colors.link + "');";
         command += "r.style.setProperty('--text-link-visited','" + result.colors.linkvisited + "');";
-        //command += "r.style.setProperty('--d2l-dropdown-background-color','" + result.colors.widgetbackground + "');";
         chrome.tabs.executeScript(tab.id, {
             code: command
-        }, _=>chrome.runtime.lastError);
+        });
     }); 
 }
